0d437fd8c8f74ef3c24a3676e53830476c82ae9a
BR <244097@niuitmo.ru>
2024-03-12 09:59:25 +0300

    Добавьте файлы проекта.

---
+++ Задача/Bank_account.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача
+{
+    internal class Bank_account<T>
+    {
+        private T Number { get; set; }
+        private int Balance { get; set; }
+        private string Name { get; set; }
+
+        public void getInfo() //метод чтения
+        {
+            Console.WriteLine($"Номер счета - {Number}, баланс - {Balance}, ФИО клиента - {Name}");
+        }
+
+        public void takeInfo() //метод заполнения
+        {
+            Console.WriteLine("Введите номер счета");
+            Number = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            Console.WriteLine("Введите баланс");
+            Balance = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите ФИО клиента");
+            Name = Convert.ToString(Console.ReadLine());
+        }
+    }
+}
---
+++ Задача/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank_account<string> account_1 = new Bank_account<string>();
+            account_1.takeInfo();
+            account_1.getInfo();
+
+            Bank_account<int> account_2 = new Bank_account<int>();
+            account_2.takeInfo();
+            account_2.getInfo();
+            Console.ReadKey();
+        }
+    }
+}
